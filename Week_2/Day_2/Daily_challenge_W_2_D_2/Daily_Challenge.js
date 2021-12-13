f24335fa95edbbdaaa67feaf332d686ad545d16e
Matanef <efratimatan@gmail.com>
2021-12-13 23:16:07 +0200

    replcaing_words

---
+++ Week_2/Day_2/Daily_challenge_W_2_D_2/Daily_Challenge.js
@@ -0,0 +1,17 @@
+let sentence = "The movie is not that bad, I like it";
+let wordNot = sentence.indexOf("not")
+let wordBad = sentence.indexOf("bad")
+
+
+let result = sentence.replace(`not that bad`, "good");
+console.log(result.toUpperCase());
+
+
+if (sentence.indexOf(wordNot) && sentence.indexOf(wordBad)) {
+	console.log(result)
+}
+
+
+if (sentence.indexOf(wordNot) && sentence.indexOf(wordBad)) {
+	console.log("The movie is good, I like it")
+}
