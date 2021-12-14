b21730ed68db13cb75868251ab6c86a9b95b7778
Matanef <efratimatan@gmail.com>
2021-12-14 20:41:05 +0200

    Exercise3_Stuck_with_repeating_when_lower_comes_after_higher

--- Week_2/Day_3/Exercise/Loop_exercise3.js
+++ Week_2/Day_3/Exercise/Loop_exercise3.js
@@ -1,15 +1,22 @@
 let userNumber = prompt("Please provide a number")
 let userInt = parseInt(userNumber)
-console.log(typeof userInt);
-console.log(userInt)
+let user10 = 10;
+// console.log(typeof userInt);
+// console.log(userInt)
+// console.log(typeof user10);
+// console.log(user10)
 
-do {
-  let userNumber = prompt ("Please provide a number");
-  	let userInt = parseInt(userNumber)
-	console.log(typeof userInt);
-	console.log(userInt)
-	} while (userInt !> 10) {
-	userNumber = prompt ("Please provide a number AGAIN");
+
+
+// for (let elements of people){
+//   console.log(elements);
+
+for (let Intprompt = userInt; Intprompt > 10; Intprompt++) {
+	if (Intprompt < user10) {
+		break;
+		} else {
+  	userNumber = prompt("Please provide a number")
+	userInt = parseInt(userNumber)
 }
-
+}
 
