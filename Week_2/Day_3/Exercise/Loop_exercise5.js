f46ac6ae5123aee4b31083220f442ab9905fedbf
Matanef <efratimatan@gmail.com>
2021-12-14 23:01:15 +0200

    Exercise5_Keys

--- Week_2/Day_3/Exercise/Loop_exercise5.js
+++ Week_2/Day_3/Exercise/Loop_exercise5.js
@@ -1,5 +1,6 @@
-let family = []
+let family = {
 	{Father : "Raphael" 
+}
 }
 	{Mother : "Shirly"
 }
@@ -8,11 +9,35 @@ let family = []
 	{Daughter : "Dana"
 }
 	{Dog : "Lanou"
-};
+}
 	
 
+for (let key in family){
+console.log("Bracket NOTATION the value is : ", family[key])
+  }
+// for (let element of family){
+// console.log(element)
+// console.log(element["family"])
+// };
 
-for (let element of family){
+// let family = [
+//   {
+//     username:"John",
+//     lastName : "Smith"
+//   },
+//   {
+//     username:"Tom",
+//     lastName : "Smith"
+//   },
+//   {
+//     username:"Lea",
+//     lastName : "Smith"
+//   }
+// ];
 
-console.log(family[element])
-};
+// //1. console.log the username of each user
+// for (let element of users){
+//   console.log(element); // element retrieve each object inside the array
+//   console.log(element["username"]) 
+// }
+
