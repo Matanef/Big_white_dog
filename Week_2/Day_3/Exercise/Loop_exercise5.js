eae766ec21afb8b23f1f9468ca9f4ba2577c5704
Matanef <efratimatan@gmail.com>
2021-12-14 22:53:17 +0200

    Exercise5

---
+++ Week_2/Day_3/Exercise/Loop_exercise5.js
@@ -0,0 +1,18 @@
+let family = []
+	{Father : "Raphael" 
+}
+	{Mother : "Shirly"
+}
+	{Son : "Matan"
+}
+	{Daughter : "Dana"
+}
+	{Dog : "Lanou"
+};
+	
+
+
+for (let element of family){
+
+console.log(family[element])
+};
