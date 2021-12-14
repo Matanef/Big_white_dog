f12d37ff63a44084c7b65b06f9d0e9db934df21c
Matanef <efratimatan@gmail.com>
2021-12-14 18:43:19 +0200

    Exercise2

---
+++ Week_2/Day_3/Exercise/Loop_exercise2.js
@@ -0,0 +1,8 @@
+let colors = ["Blue", "Red", "Orange", "Green", "Magenta"]
+console.log(colors)
+
+
+
+for (let n = 0; n < colors.length ; n ++){
+    console.log(`My #${n+1} Choise ${colors[n]}`)
+}
