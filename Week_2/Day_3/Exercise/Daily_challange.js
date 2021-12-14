ef5a04dcc804518fa0808941074f6d4ff836861f
Matanef <efratimatan@gmail.com>
2021-12-14 21:47:39 +0200

    Exercise4_+_Daily_C

---
+++ Week_2/Day_3/Exercise/Daily_challange.js
@@ -0,0 +1,9 @@
+let Shape2 = ["*"]
+for (let n = 0; n <= 6; n++){
+		if (n==6) {
+			break
+		} else {		
+		console.log(Shape2*n)
+
+}
+}
---
+++ Week_2/Day_3/Exercise/Loop_exercise4.js
@@ -0,0 +1,18 @@
+let building = {
+    numberOfFloors : 4,
+    numberOfAptByFloor : {
+        firstFloor : 3,
+        secondFloor : 4,
+        thirdFloor : 9,
+        fourthFloor : 2,
+    },
+    nameOfTenants : ["Sarah", "Dan", "David"],
+    numberOfRoomsAndRent:  {
+        sarah: [3, 990],
+        dan :  [4, 1000],
+        david : [1, 500],
+    },
+}
+
+for (let elem of building)
+    console.log(elem["numberOfFloors"])
