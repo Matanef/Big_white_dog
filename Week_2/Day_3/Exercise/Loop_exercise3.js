c27365bc1b54fd2fe449cdfe9e63fd18a0547e9a
Matanef <efratimatan@gmail.com>
2021-12-14 19:57:12 +0200

    Exercise3_draft

---
+++ Week_2/Day_3/Exercise/Loop_exercise3.js
@@ -0,0 +1,15 @@
+let userNumber = prompt("Please provide a number")
+let userInt = parseInt(userNumber)
+console.log(typeof userInt);
+console.log(userInt)
+
+do {
+  let userNumber = prompt ("Please provide a number");
+  	let userInt = parseInt(userNumber)
+	console.log(typeof userInt);
+	console.log(userInt)
+	} while (userInt !> 10) {
+	userNumber = prompt ("Please provide a number AGAIN");
+}
+
+
