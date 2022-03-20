1f64092ff52afba2dc42f9b435d3bb26975f03bd
Matanef <efratimatan@gmail.com>
2022-03-20 16:45:52 +0200

    daily challenge2 defined a var for the first and second array, and then accessed the oranges as an inner array

--- Full_Stack_Python/DI-Bootcamp/Week_2/Day_1/Daily_Challenge/Exercise_2_daily_challenge/script.js
+++ Full_Stack_Python/DI-Bootcamp/Week_2/Day_1/Daily_Challenge/Exercise_2_daily_challenge/script.js
@@ -1,1 +1,14 @@
 let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(moreFruits);
+
+let firstNestedArray = moreFruits[0];
+let secondNestedArray = moreFruits[1];
+
+console.log(secondNestedArray);
+
+let internalArray = secondNestedArray[1];
+console.log(internalArray);
+
+
+
+
