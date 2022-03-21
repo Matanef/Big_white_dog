473421237cf64db117c32af1d8456ea53645485e
Matanef <efratimatan@gmail.com>
2022-03-21 15:26:54 +0200

    Daily Challenge done

--- Full_Stack_Python/DI-Bootcamp/Week_2/Day_2/Daily_Challenge/script.js
+++ Full_Stack_Python/DI-Bootcamp/Week_2/Day_2/Daily_Challenge/script.js
@@ -1,10 +1,25 @@
 let sentence = "The movie is not that bad, I like it";
 let wordNot = sentence.indexOf("not")
+console.log(wordNot)
 let wordBad = sentence.indexOf("bad")
+console.log(wordBad)
+
+let arr = sentence.split(` `)
+console.log(arr)
 
 
-// let result = sentence.replace(`not that bad`, "good");
-// console.log(result.toUpperCase());
+let result = sentence.replace(`not that bad`, "good");
+console.log(`${result.toUpperCase()}, this is just a practice and an example for my self`);
+
+if (wordNot<wordBad){
+	console.log(result)
+} else if (wordNot<wordBad){
+	console.log(sentence)
+}else if (wordNot=-1, wordBad=-1){
+	console.log("The movie is not that bad, I like it")
+}
+
+
 
 
 // if (sentence.indexOf(wordNot) && sentence.indexOf(wordBad)) {
@@ -12,8 +27,11 @@ let wordBad = sentence.indexOf("bad")
 // }
 
 
-if (sentence.indexOf(wordNot) && sentence.indexOf(wordBad)) {
-	console.log("The movie is good, I like it")
-} else if(false) {
-	console.log(sentence)
-}
+// if (sentence.indexOf(wordNot) && sentence.indexOf(wordBad)) {
+// 	console.log("The movie is good, I like it")
+// } else if(false) {
+// 	console.log(sentence)
+// }
+
+// let arr = sentence.split(``)
+// console.log(arr)
