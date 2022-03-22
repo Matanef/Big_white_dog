444b3d77bfa46a3e838d02689209d4faf5343302
Matanef <efratimatan@gmail.com>
2022-03-22 17:34:26 +0200

    frustrating exercise6 first and last commit

---
+++ Full_Stack_Python/DI-Bootcamp/Week_2/Day_3/Loops/Exercise_submit/ExerciseXP/exercise6/script.js
@@ -0,0 +1,14 @@
+let details = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'raindeer'
+}
+
+
+for (let [x, value] of Object.entries(details)){
+
+  console.log(`${x}, ${value}`)}
+
+
+  console.log(details);
+  //i know it's just a cheat but, the wrods are aligned for a row :-)
