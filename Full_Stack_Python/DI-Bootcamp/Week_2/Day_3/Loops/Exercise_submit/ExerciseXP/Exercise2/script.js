8e672b0d976969bfb64a3424e9d2ef6d13b48b69
Matanef <efratimatan@gmail.com>
2022-03-22 13:58:41 +0200

    exercise2 first commit done

---
+++ Full_Stack_Python/DI-Bootcamp/Week_2/Day_3/Loops/Exercise_submit/ExerciseXP/Exercise2/script.js
@@ -0,0 +1,13 @@
+let colors = ["red", "blue", "black", "yellow", "green"]
+
+let colorsid = colors.length
+console.log(colorsid)
+
+for (let i = 0; i <= colorsid -1; i++)
+	console.log(`My ${i+1}# choise is ${colors[i]}`);
+
+
+// for (let elemnts of colors) {
+// 	console.log(elemnts.indexOf())
+// 	console.log(`My ${elemnts.indexOf()} choise is ${elemnts}`);
+// }
