5042e25443887b7ceb7b1ed272c3c15bb3ea392d
Matanef <efratimatan@gmail.com>
2022-03-23 23:39:10 +0200

    daily challenge nest not working

--- Full_Stack_Python/DI-Bootcamp/Week_2/Day_4/Exercise_submit/Daily_Challenge/script.js
+++ Full_Stack_Python/DI-Bootcamp/Week_2/Day_4/Exercise_submit/Daily_Challenge/script.js
@@ -6,18 +6,20 @@ for (let i = 1; i <= 4; i++){
 
 console.log(wordListArr)
 let wordStr =  wordListArr.join(", ");
-console.log(wordStr);
+console.log(wordStr); 
 let brk = wordStr.split(` `);
 let res = brk.join(" ")
 console.log(res);
 
 let longestWord = 0
 for (let l = 0; l <brk.length; l++){
-    if(brk[l].length > longestWord){
-        longestWord = brk[l].length;
+    for (let nest =0; nest <=l; nest++ )
+        if(nest.length > longestWord){
+            longestWord = brk[l].length;
+            console.log(wordListArr[l] + "*");
+        }
     }
-}
-console.log(longestWord)
+    console.log(longestWord)
 
 
 
