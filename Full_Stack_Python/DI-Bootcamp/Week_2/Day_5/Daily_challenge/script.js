5bd6b21e06f265a7c92b6d4f98814fd828baf345
Matanef <efratimatan@gmail.com>
2022-03-27 05:25:29 +0300

    daily challenge fixed counter issue

--- Full_Stack_Python/DI-Bootcamp/Week_2/Day_5/Daily_challenge/script.js
+++ Full_Stack_Python/DI-Bootcamp/Week_2/Day_5/Daily_challenge/script.js
@@ -1,26 +1,30 @@
 let userNum;
 do {
 
-	userNum = parseInt(prompt('type a real number'));
+	userNum = parseInt(prompt('How many bottles?'));
 } while (userNum <= 0 || isNaN(userNum));
 
-let counter = 1; // each time 1 bottle falls
+let counter = 1; 
 
 const sing = (num) => {
 	for (let i = 0; i < num; i++) {
-		let poun = '';
+		let pnoun = '';
 		counter === 1 ? pnoun = 'it' : pnoun = 'them';
+
 		console.log(`${num} bottles of beer on the wall`);
 		console.log(`${num} botles of beer`);
 		console.log(`Take ${counter} down, pass ${pnoun} around`);
+
 		num = userNum - counter;
 		counter++;
+
 		console.log(`${num} bottles of beer on the wall`);
 		console.log('\n');
+		
 		if (num - counter < 0 && num > 0) {
 			console.log(`${num} bottles of beer on the wall`);
 			console.log(`${num} botles of beer`);
-			// counter = num;
+			counter = num;
 			console.log(`Take ${counter} down, pass ${pnoun} around`);
 			if (num - counter <= 0)
 				console.log("no bottle of beer on the wall");
