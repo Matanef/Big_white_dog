c2bef120787ffe822a679ac780803dcb37cc72ec
Matanef <efratimatan@gmail.com>
2022-03-31 01:50:42 +0300

    creating button function to add rows, something is being added, not rows but something

--- Full_Stack_Python/DI-Bootcamp/Week_3/Day_2/Course_notes/Exercise_DI/1/script.js
+++ Full_Stack_Python/DI-Bootcamp/Week_3/Day_2/Course_notes/Exercise_DI/1/script.js
@@ -1,19 +1,29 @@
-let tableElement = document.getElementById("sampleTable");
-console.log(tableElement);
+// let tableElement = document.getElementById("sampleTable");
+// console.log(tableElement);
 
-let trElement = document.getElementsByTagName('tr')
-
-let tdElement = document.getElementsByTagName('td')
-console.log(tdElement);
+// let trElement = document.getElementsByTagName('tr')
+// console.log(trElement);
+// let tdElement = document.getElementsByTagName('td')
+// console.log(tdElement);
 
 function insertRow (){
-	let tr = document.createElement('tr');
+	let trElement = document.createElement('tr');
 	let rowNum = document.getElementsByTagName("tr").length
 	// let cellNum = 
-	let cellText = document.createTextNode
-	let tableFunc = document.getElementById("sampleTable");
-	let trFunc = document.getElementsByTagName('tr');
-	let tdElement = document.getElementsByTagName('td')
-	let tdText = document.createTextNode(`Row ${tdElement.length} cell1`)
-}
+
+	for (let i =0; i<trElement.length; i++){
+		let tdElement = document.createElement("td");
+		let tdText = document.createTextNode(`Row ${tdElement.length} cell1`);
+		tdElement.appendChild(tdText);
+		trElement.appendChild(tdElement);
+	}
+	let tableElement = document.getElementById("sampleTable");
+	tableElement.appendChild(trElement)
+	console.log(tableElement);
+
+	// let cellText = document.createTextNode
+	// let tableFunc = document.getElementById("sampleTable");
+	// let trFunc = document.getElementsByTagName('tr');
+	// let tdElement = document.getElementsByTagName('td')
+	// let tdText = document.createTextNode(`Row ${tdElement.length} cell1`)
 };
