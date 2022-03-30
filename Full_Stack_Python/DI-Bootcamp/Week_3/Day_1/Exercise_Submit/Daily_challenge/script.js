c81fe084f0bd42b8aeb2de103de81aeba7603da8
Matanef <efratimatan@gmail.com>
2022-03-30 19:37:07 +0300

    adjusted some colors of the planets, going out with the dog

--- Full_Stack_Python/DI-Bootcamp/Week_3/Day_1/Exercise_Submit/Daily_challenge/script.js
+++ Full_Stack_Python/DI-Bootcamp/Week_3/Day_1/Exercise_Submit/Daily_challenge/script.js
@@ -1,13 +1,13 @@
 let solarArray = [
-{name: 'Mercury', moons: 0, color: "lightgrey"},
-{name: 'Venus', moons: 0, color: "lightgrey"},
+{name: 'Mercury', moons: 0, color: "#c8c3cc"},
+{name: 'Venus', moons: 0, color: "#ff7b25"},
 {name: 'Earth', moons: 1, color: "blue"},
 {name: 'Mars', moons: 2, color: "#c94c4c"},
 {name: 'Jupiter', moons: 79, color: "#bc5a45"},
 {name: 'Saturn', moons: 82, color: "#dac292"},
-{name: 'Uranus', moons: 27, color: "lightblue"},
-{name: 'Neptun', moons: 14, color: "darkblue"},
-{name: 'Pluto', moons: 5, color: "lightgrey"}
+{name: 'Uranus', moons: 27, color: "#b7d7e8"},
+{name: 'Neptun', moons: 14, color: "#034f84"},
+{name: 'Pluto', moons: 5, color: "#cab577"}
 ]
 
 let planetSection = document.querySelector('section');
