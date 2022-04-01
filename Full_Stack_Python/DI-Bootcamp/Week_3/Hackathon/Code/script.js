5a5b3004d2f2e83804242c18467f1993fbaeba65
Matanef <efratimatan@gmail.com>
2022-04-01 21:45:02 +0300

    added script for random mathequation but don't get the concept

---
+++ Full_Stack_Python/DI-Bootcamp/Week_3/Hackathon/Code/script.js
@@ -0,0 +1,15 @@
+let signsArr = [
+{sign: "+"},
+{sign: "-"},
+{sign: "/"},
+{sign: "*"}
+]
+
+// let numArr = [
+// ]
+
+let paraElement = document.getElementsByClassName("container");
+let newElement = document.createElement('div');
+
+
+// function mathProb(){
