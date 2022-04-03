bae5045f66755a8230a68ebdc505b3c7dc05f927
Matanef <efratimatan@gmail.com>
2022-04-03 03:24:58 +0300

    exercise_4 working

--- Full_Stack_Python/DI-Bootcamp/Week_3/Day_2/Exercise_Submit/Exercises/Exercise_4/script.js
+++ Full_Stack_Python/DI-Bootcamp/Week_3/Day_2/Exercise_Submit/Exercises/Exercise_4/script.js
@@ -1,15 +1,14 @@
-	
+let submitForm = document.getElementById("submit")
+submitForm.addEventListener("click", volumeOfSphere)
 
 function volumeOfSphere(){
-	let volume = 0;
-	let radius = document.getElementById("radius").value;
+	event.preventDefault()
+	let radiusValue = document.getElementById("radius").value;
 	// for (let i = 0; i=2; i++ ){
 	// 	let radiusHigh = radius[i];
-	let volume = 4/3 * Math.PI * Math.pow(radius, 3);
-	document.getElementById("volume").value;
+	let equation = 4/3 * Math.PI * Math.pow(radiusValue, 3);
+	let volumeres = document.getElementById("volume");
+	volumeres.value = equation
 
-	return volume
 
 }
-let volume = volumeOfSphere()
-console.log(volume)
