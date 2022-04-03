0a4e11fa7736d7162e9f508a43fbabaa37f49e41
Matanef <efratimatan@gmail.com>
2022-04-03 03:02:37 +0300

    exercise_4 volume is declerd of error for sign

--- Full_Stack_Python/DI-Bootcamp/Week_3/Day_2/Exercise_Submit/Exercises/Exercise_4/script.js
+++ Full_Stack_Python/DI-Bootcamp/Week_3/Day_2/Exercise_Submit/Exercises/Exercise_4/script.js
@@ -1,12 +1,12 @@
 	
 
 function volumeOfSphere(){
-	let volume;
+	let volume = 0;
 	let radius = document.getElementById("radius").value;
 	// for (let i = 0; i=2; i++ ){
 	// 	let radiusHigh = radius[i];
 	let volume = 4/3 * Math.PI * Math.pow(radius, 3);
-	
+	document.getElementById("volume").value;
 
 	return volume
 
