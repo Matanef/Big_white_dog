8108a1a76767c5ea185430600a549b7667f1e33b
Matanef <efratimatan@gmail.com>
2022-04-03 15:39:18 +0300

    daily challenge done

---
+++ Full_Stack_Python/DI-Bootcamp/Week_4/Day_1/Daily_Challenge/Daily_challenge.py
@@ -0,0 +1,18 @@
+userString = input("Please Enter a string with percisely 10 letters: ")
+print(len(userString))
+first_char = userString[0]
+last_char = userString[-1]
+
+
+
+if len(userString) < 10:
+    print("string not long enough.")
+elif len(userString) > 10:
+    print("string too long.")
+else:
+    print(first_char, last_char)
+
+emptyString = ""
+for x in userString:
+    emptyString+=x
+    print(emptyString)
