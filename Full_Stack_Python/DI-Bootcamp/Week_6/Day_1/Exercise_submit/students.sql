2e7cf1fa212e69a1f682b46a1e99c68a162e0009
Matanef <efratimatan@gmail.com>
2022-04-25 03:35:58 +0300

    students exercixe not done

---
+++ Full_Stack_Python/DI-Bootcamp/Week_6/Day_1/Exercise_submit/students.sql
@@ -0,0 +1,5 @@
+-- select * from students;
+-- select first_name, last_name from students;
+-- select first_name, last_name from students where student_id = 2;
+-- select first_name, last_name from students where last_name = 'Benichou' and first_name = 'Marc';
+-- select first_name, last_name from students where last_name = 'Benichou' or first_name = 'Marc'
