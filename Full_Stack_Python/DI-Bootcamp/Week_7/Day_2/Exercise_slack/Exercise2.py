fbf32255a32d03c74b26366d64698189b98e303d
Matanef <efratimatan@gmail.com>
2022-05-03 03:17:30 +0300

    Exercise2 from slack 02052022 question 5

--- Full_Stack_Python/DI-Bootcamp/Week_7/Day_2/Exercise_slack/Exercise2.py
+++ Full_Stack_Python/DI-Bootcamp/Week_7/Day_2/Exercise_slack/Exercise2.py
@@ -6,4 +6,9 @@ chipo = pd.read_table('https://raw.githubusercontent.com/justmarkham/DAT8/master
 chipo["item_price"] = chipo["item_price"].str.replace("$", "").astype("float64")
 biggerThenTen = chipo[chipo["item_price"] > 10.00]["item_price"].count()
 print(biggerThenTen)
+print('\n')
 
+price = chipo[["item_name", "item_price"]]
+print("Price of each item")
+print(price)
+print('\n')
