69db0cb71bd281cb1b9767f11134e8bdb904ebd8
Matanef <efratimatan@gmail.com>
2022-05-03 03:08:28 +0300

    Exercise2 from slack 02052022 question 4

---
+++ Full_Stack_Python/DI-Bootcamp/Week_7/Day_2/Exercise_slack/Exercise2.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+chipo = pd.read_table('https://raw.githubusercontent.com/justmarkham/DAT8/master/data/chipotle.tsv')
+
+chipo["item_price"] = chipo["item_price"].str.replace("$", "").astype("float64")
+biggerThenTen = chipo[chipo["item_price"] > 10.00]["item_price"].count()
+print(biggerThenTen)
+
