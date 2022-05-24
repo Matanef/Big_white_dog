d3cacfa995508cffa092380aa89716c26db5fe2d
Matanef <efratimatan@gmail.com>
2022-05-24 11:27:50 +0300

    continue of the BTRE project

--- Full_Stack_Python/DI-Bootcamp/Venvs/btre_env/btre_root/pages/views.py
+++ Full_Stack_Python/DI-Bootcamp/Venvs/btre_env/btre_root/pages/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
